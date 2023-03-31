078459aa3b376edf3d833deb87505f3b3f099980
tanphatngo12 <tanphat2002125@gmail.com>
2023-03-31 14:28:15 +0700

    Add project files.

---
+++ bai3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so x: ");
+            int x = int.Parse(Console.ReadLine());
+            if (x%3==0)
+            {
+                Console.WriteLine("x chia het cho 3");
+            }
+            else
+            {
+                Console.WriteLine("x khong chia het cho 3");
+            }
+            Console.ReadLine();
+        }
+    }
+}
